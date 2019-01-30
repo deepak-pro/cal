ad4a774c3f8cefdd9d4d1b5378b49a451bfd89e9
Deepak JOSHI <joshideepak4598@gmail.com>
2019-01-30 09:10:46 +0530

    Day of week implemented

--- calcu.c
+++ calcu.c
@@ -1,10 +1,59 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+
+bool checkLeapYear(int year){
+	if((year%4 == 0 && year%100 !=0) || year%400 == 0){
+		return true ;
+	}else {
+		return false ;
+	}
+}
+
+int dayOfWeek(int k ,int m , int y){
+
+	m = m - 2 ;
+	if(m == 0){
+		m = 12 ;
+		y = y - 1 ;
+	}
+	if(m == -1){
+		m = 11 ;
+		y = y - 1 ;
+	}
+	int d = y % 100 ;
+	int c = y / 100 ;
+
+	//printf("%d %d %d %d \n", k,m,d,c);
+
+	int f = k + ((13*m - 1)/5) + d + (d/4) + (c/4) - 2*c ;
+
+	//printf("f is %d\n", f );
+    int dow ;
+    if(f<0){
+    	dow = (f%7) + 7 ;
+    }else {
+    	dow = f % 7 ;
+    }
+    //printf("dow is %d\n", dow );
+
+	return dow;
+}
 
 int main(){
-	
+
 	int d , m , y ;
 	scanf("%d %d %d" , &d , &m , &y);
+
+
+	if(checkLeapYear(y)){
+		printf("Year is a leap year\n");
+	}else {
+		printf("Year is not a leap year\n");
+	}
+
+
+
 	int nod ;
 	switch(m){
 		case 1 : printf("    January %d\n" , y);
@@ -50,7 +99,7 @@ int main(){
 	printf("Su Mo Tu We Th Fr Sa\n") ;
 
 	int counter = 1 ;
-	int dow = 3 ;
+	int dow = dayOfWeek(1,m,y) + 1 ;
 	while(counter<=nod){
 		if(dow == 8){
 			printf("\n");
